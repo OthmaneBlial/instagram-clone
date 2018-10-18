6eae4227d3ab3aa34bc20c001d8e8fb63143da6f
Othmane <blial.othmane@gmail.com>
2018-10-18 22:08:34 +0000

    Add styled components and create the button component

---
+++ src/components/button/index.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import Button from './style.js'
+
+export default ({ children }) => <Button>{ children }</Button>
+
---
+++ src/components/button/style.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+const Button = styled.a`
+    text-decoration: none;
+    background-color: #3897f0;
+    border: 1px solid #3897f0;
+    border-radius: 4px;
+    color: #fff;
+    max-width: 240px;
+    padding: 3px 7px;
+    font-size: 13px;
+`
+
+export default Button
